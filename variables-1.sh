cc517fe1acea8d260ac947bb23c6d39b143aaa76
umamahesh369 <bunny707505@gmail.com>
2025-06-21 17:22:23 +0530

    added

---
+++ variables-1.sh
@@ -0,0 +1,7 @@
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1:: hello $PERSON2, how are you"
+echo "$PERSON2:: i am fine $PERSON1,what about you"
+echo "$PERSON1:: i am also fine ,what about your work"
+echo "$PERSON2:: my work also good"
