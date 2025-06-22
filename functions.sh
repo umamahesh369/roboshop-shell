c50aee4274dffb533dfe2654af95d465d6e851dd
umamahesh369 <bunny707505@gmail.com>
2025-06-22 17:12:16 +0530

    added

--- functions.sh
+++ functions.sh
@@ -1,15 +1,15 @@
 ID=$(id -u)
 VALIDATE(){
-    if ($? -ne 0)
-    then  
+    if [[ $ID -ne 0 ]]; then
+ 
        echo "ERROR:: installing fail"
        exit 1
     else
        echo"intalling success"
     fi
 }
-   if ($ID -ne 0)
-   then 
+   if [[ $ID -ne 0 ]]; then
+
       echo "ERROR:: please run the script with root access"
       exit 1
    else
@@ -17,8 +17,8 @@ VALIDATE(){
    fi
 
    apt install mysql -y
-   VALIDATE$? "Installing mysql"
+   VALIDATE $? "Installing mysql"
 
    apt install git -y
-   VALIDATE$? "Installing git"
+   VALIDATE $? "Installing git"
   
