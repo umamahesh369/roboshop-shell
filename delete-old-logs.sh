be9e99d6a02ad9594a32d5e5b6779c76b82bc4b6
umamahesh369 <bunny707505@gmail.com>
2025-06-28 15:34:10 +0530

    added

---
+++ delete-old-logs.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+SOURCE_DIR= /tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [-d $SOURCE_DIR]
+then 
+  echo -e ""$G Source directory exists $N"
+else
+  echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+  exit 1
+fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while  IFS= read -r line
+do
+  echo "deleting file: $line
+  rm -rf $line
+done <<< $FILES
+
+
+
